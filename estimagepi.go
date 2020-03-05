2a5c4f8dae7aeaf038c540248b45154f55401bd4
Luke Kelly <lukejelly@outlook.com>
2020-03-05 13:18:24 -0800

    ini

---
+++ estimagepi.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	mr "math/rand"
+	t "time"
+)
+func main() {
+	var num int
+	fmt.Print("Input how many points you would like to use: ")
+	fmt.Scan(&num)
+	fmt.Println(estimatePi(num))
+}
+
+func estimatePi(n int) float64{
+	var numPointsInCircle float64 = 0
+	var numPointsInSquare float64 = float64(n)
+	mr.Seed(t.Now().UnixNano())
+	for i := 0; i < n; i++ {
+		pointX := mr.Float32()
+		pointY := mr.Float32()
+		distance := (pointX*pointX)+(pointY*pointY)
+		
+		if (distance < 1) {
+			numPointsInCircle++
+		}
+	}
+	return 4 * (numPointsInCircle/numPointsInSquare)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module /home/luke/programming/Golang/estimatepi
+
+go 1.14
