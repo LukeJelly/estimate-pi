9846f1f8eeeb8af8c0a5ff44094543fe150c5fdf
Luke Kelly <lukejelly@outlook.com>
2020-03-05 13:29:57 -0800

    comments

--- estimagepi.go
+++ estimagepi.go
@@ -1,3 +1,7 @@
+/*
+ *Prompt: Using only basic arithmetic and a random number generator approximate
+ *Pi.
+ */
 package main
 
 import (
@@ -5,6 +9,7 @@ import (
 	mr "math/rand"
 	t "time"
 )
+
 func main() {
 	var num int
 	fmt.Print("Input how many points you would like to use: ")
@@ -12,18 +17,18 @@ func main() {
 	fmt.Println(estimatePi(num))
 }
 
-func estimatePi(n int) float64{
+func estimatePi(n int) float64 {
 	var numPointsInCircle float64 = 0
 	var numPointsInSquare float64 = float64(n)
 	mr.Seed(t.Now().UnixNano())
 	for i := 0; i < n; i++ {
 		pointX := mr.Float32()
 		pointY := mr.Float32()
-		distance := (pointX*pointX)+(pointY*pointY)
-		
-		if (distance < 1) {
+		distance := (pointX * pointX) + (pointY * pointY)
+
+		if distance < 1 {
 			numPointsInCircle++
 		}
 	}
-	return 4 * (numPointsInCircle/numPointsInSquare)
+	return 4 * (numPointsInCircle / numPointsInSquare)
 }
